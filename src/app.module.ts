6cb7dc2ae053aec066528285b8a0951cf7bca452
Cesar Oliveira <cesar.oliveira@yalo.com.br>
2021-11-18 08:41:35 -0300

    Aplicação Finalizada

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+
+import { AppService } from './app.service';
+
+import { StudentsModule } from './modules/students/students.module';
+import { DisciplinesModule } from './modules/disciplines/disciplines.module';
+import { LessonsModule } from './modules/lessons/lessons.module';
+import { ContentsModule } from './modules/contents/contents.module';
+
+@Module({
+  imports: [
+    DisciplinesModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    TypeOrmModule.forRoot(),
+    StudentsModule,
+    DisciplinesModule,
+    LessonsModule,
+    ContentsModule,
+  ],
+  controllers: [],
+  providers: [AppService],
+})
+export class AppModule {}
